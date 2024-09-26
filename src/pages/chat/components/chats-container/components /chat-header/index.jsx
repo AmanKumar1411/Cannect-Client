532f28296fe0803b785425ea0f63c02a3894c680
AmanKumar <itsamankumar786@gmail.com>
2024-09-27 03:42:36 +0530

    change

--- src/pages/chat/components/chats-container/components /chat-header/index.jsx
+++ src/pages/chat/components/chats-container/components /chat-header/index.jsx
@@ -1,14 +1,19 @@
+import { useAppStore } from "@/stores";
 import { RiCloseFill } from "react-icons/ri";
 
 const ChatHeader = () => {
+  const { closeChat } = useAppStore();
   return (
     <header className="h-[10vh] border-b-2 border-[white] flex items-center justify-between px-20">
       <div className="flex items-center gap-5">
         <div className="flex items-center justify-center gap-3"></div>
         <div className="flex items-center justify-center gap-5">
-          {/* <button className="text-neutral-500   focus:border-none focus:outline-none focus:text-white duration-300 transition-all">
+          <button
+            className="text-neutral-500   focus:border-none focus:outline-none focus:text-white duration-300 transition-all"
+            onClick={closeChat}
+          >
             <RiCloseFill className="text-3xl" />
-          </button> */}
+          </button>
         </div>
       </div>
     </header>
